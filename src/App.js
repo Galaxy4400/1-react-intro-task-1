32b597529b1085fc38b86ce0c90609422a0aac90
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-06-17 19:32:43 +0300

    App.js without JSX

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', null, new Date().getFullYear()),
+		),
 	);
 };
-
-// На мой взгляд, здесь всё относится к декларативному стилю,
-// может быть только за исключением конкретного текста на 10 и 18 строке,
-// где мы императивно его указываем.
-// Но этот момент тоже можно свести к декларативности, если допустим текс приходит с сервера,
-// и мы его вставляем через какой-то функционал.
